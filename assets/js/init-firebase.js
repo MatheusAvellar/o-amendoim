2c3964b0ca961a328ebdb95cb622d643b43439b9
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2020-08-04 00:54:20 -0300

    Atomicize db access

--- assets/js/init-firebase.js
+++ assets/js/init-firebase.js
@@ -1,16 +1,3 @@
-const like_btn = document.getElementById("btn-like-post");
-const like_txt = document.getElementById("like-count");
-const meh_btn = document.getElementById("btn-meh-post");
-const meh_txt = document.getElementById("meh-count");
-const dislike_btn = document.getElementById("btn-dislike-post");
-const dislike_txt = document.getElementById("dislike-count");
-
-let id = "";
-let like_count = 0;
-let meh_count = 0;
-let dislike_count = 0;
-let total_count = 0;
-
 firebase.initializeApp({
   apiKey: "AIzaSyCCkQQR_bwPyXOa5uo_uavYK8UQVkD0Ylk",
   authDomain: "avellar-website.firebaseapp.com",
@@ -19,31 +6,46 @@ firebase.initializeApp({
 
 const db = firebase.firestore();
 
+const like_txt = document.getElementById("like-count");
+const meh_txt = document.getElementById("meh-count");
+const dislike_txt = document.getElementById("dislike-count");
+
+let id = "";
+
+const btns = {
+  likes: document.getElementById("btn-like-post"),
+  mehs: document.getElementById("btn-meh-post"),
+  dislikes: document.getElementById("btn-dislike-post")
+};
+const counts = {
+  likes: 0,
+  mehs: 0,
+  dislikes: 0,
+  total: 0
+};
+
 function updateLabels() {
-  like_txt.textContent = like_count;
+  like_txt.textContent = counts.likes;
   const lk = document.createElement("div");
-  lk.textContent = `(${getPctg(like_count)}%)`;
+  lk.textContent = `(${getPctg(counts.likes)}%)`;
   like_txt.appendChild(lk);
 
-  meh_txt.textContent = meh_count;
+  meh_txt.textContent = counts.mehs;
   const mh = document.createElement("div");
-  mh.textContent = `(${getPctg(meh_count)}%)`;
+  mh.textContent = `(${getPctg(counts.mehs)}%)`;
   meh_txt.appendChild(mh);
 
-  dislike_txt.textContent = dislike_count;
+  dislike_txt.textContent = counts.dislikes;
   const dl = document.createElement("div");
-  dl.textContent = `(${getPctg(dislike_count)}%)`;
+  dl.textContent = `(${getPctg(counts.dislikes)}%)`;
   dislike_txt.appendChild(dl);
 }
 
 function getPctg(v) {
-  const res = Math.round(v / total_count * 100 * 100) / 100;
+  const res = Math.round(v / counts.total * 100 * 100) / 100;
   return isNaN(res) ? 0 : res;
 }
 
-function getDate() {
-  return firebase.firestore.Timestamp.fromDate(new Date());
-}
 const path = location.pathname.replace(/\//g,"-").substr(1);
 const date = path.substr(0,10);
 
@@ -51,68 +53,51 @@ db.collection("blog").where("date","==",date).get().then((querySnapshot) => {
   querySnapshot.forEach((doc) => {
     id = doc.id;
     const obj = doc.data();
-    total_count = 0;
+    counts.total = 0;
 
-    like_count = obj.likes;
-    total_count += like_count;
+    counts.likes = obj.likes;
+    counts.total += counts.likes;
 
-    meh_count = obj.mehs;
-    total_count += meh_count;
+    counts.mehs = obj.mehs;
+    counts.total += counts.mehs;
 
-    dislike_count = obj.dislikes;
-    total_count += dislike_count;
+    counts.dislikes = obj.dislikes;
+    counts.total += counts.dislikes;
 
     updateLabels();
   });
 });
 
-function disableButtons() {
-  like_btn.disabled = true;
-  meh_btn.disabled = true;
-  dislike_btn.disabled = true;
-}
+// Adiciona click event handlers
+["likes", "mehs", "dislikes"].forEach(e => {
+  btns[e].onclick = () => {
+    // Caso não tenhamos ID, woopsie daisy, temos um problema
+    // Quem sabe ele cria sozinho caso não exista :) Torceremos
+    if(!id) id = path;
 
-like_btn.onclick = function() {
-  if(!id) return;
-  disableButtons();
-  like_count++;
-  total_count++;
-  like_btn.classList.add("selected");
-  updateLabels();
-  db.collection("blog").doc(id).set({
-    "likes": like_count,
-    "last_update": getDate()
-  },{ merge: true }).then(success).catch(error);
-}
-meh_btn.onclick = function() {
-  if(!id) return;
-  disableButtons();
-  meh_count++;
-  total_count++;
-  meh_btn.classList.add("selected");
-  updateLabels();
-  db.collection("blog").doc(id).set({
-    "mehs": meh_count,
-    "last_update": getDate()
-  },{ merge: true }).then(success).catch(error);
-}
-dislike_btn.onclick = function() {
-  if(!id) return;
-  disableButtons();
-  dislike_count++;
-  total_count++;
-  dislike_btn.classList.add("selected");
-  updateLabels();
-  db.collection("blog").doc(id).set({
-    "dislikes": dislike_count,
-    "last_update": getDate()
-  },{ merge: true }).then(success).catch(error);
-}
+    // Desabilita botões
+    btns.likes.disabled = true;
+    btns.mehs.disabled = true;
+    btns.dislikes.disabled = true;
 
-function success() {
-  console.log("Document successfully written");
-}
+    // Incrementa o valor local do botão clicado
+    counts[e]++;
+    // Incrementa o total
+    counts.total++;
+    // Atualiza os textos
+    updateLabels();
+    // Marca o botão clicado, pra ser estilizado diferentemente
+    btns[e].classList.add("selected");
+    // Incrementa o valor do banco
+    incrementValue(id, e);
+  }
+});
 
-function error(err) {
-  console.error("Error adding document: ", err);
+// Incrementar atomicamente, evita race condition etc
+// [Ref] firebase.google.com/docs/firestore/manage-data/add-data#increment_a_numeric_value
+function incrementValue(id, field) {
+  db.collection("blog").doc(id).update({
+    [field]: firebase.firestore.FieldValue.increment(1),
+    last_update: firebase.firestore.Timestamp.fromDate(new Date())
+  });
 }
